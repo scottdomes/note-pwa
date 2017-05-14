d867c87ad64c6f0244ea3b0d21767af6783d575b
Scott Domes <scottdomes@Scotts-MacBook-Air.local>
2017-05-14 22:31:15 +0200

    Displayed note list

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import firebase from 'firebase';
 
 export default class ListContainer extends Component {
+	state = { notes: [] }
+
 	componentDidMount() {
-		firebase.database().ref('/notes').once('value').then(function(snapshot) {
-		  console.log(snapshot.val())
+		firebase.database().ref('/notes').once('value').then((snapshot) => {
+			const notes = []
+			snapshot.forEach(childSnapshot => {
+				const value = childSnapshot.val()
+				value.id = childSnapshot.key
+		    notes.push(value);
+		  });
+		  this.setState({ notes })
 		});
 	}
 
@@ -12,6 +20,16 @@ export default class ListContainer extends Component {
 		return (
 			<div>
 				<h1>ListContainer</h1>
+				{
+					this.state.notes.map(note => {
+						return (
+							<div key={note.id}>
+								<h3>{note.title}</h3>
+								<p>{note.body}</p>
+							</div>
+						)
+					})
+				}
 			</div>
 		);
 	}
