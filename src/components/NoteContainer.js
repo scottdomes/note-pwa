e17d95b32625a73f8dce9f29c1dfcd2e3580f727
Scott Domes <scottdomes@Scotts-MacBook-Air.local>
2017-05-14 22:18:09 +0200

    Default configuration for loading a note

--- src/components/NoteContainer.js
+++ src/components/NoteContainer.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 export default class NoteContainer extends Component {
+	componentDidMount() {
+		firebase.database().ref('/notes/' + 1).once('value').then(function(snapshot) {
+		  console.log(snapshot.val())
+		});
+	}
+
 	render() {
 		return (
 			<div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import App from './App';
 import './index.css';
+import SECRETS from './config/secrets';
+
+const config = {
+  apiKey: SECRETS.firebase,
+  authDomain: "note-pwa-d880f.firebaseapp.com",
+  databaseURL: "https://note-pwa-d880f.firebaseio.com",
+  projectId: "note-pwa-d880f",
+  storageBucket: "note-pwa-d880f.appspot.com",
+  messagingSenderId: "469751265888"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
